7f7c27c2d3f854dde45ddcd9735dc87cfd24834d
Kamil Mrowiec <kmrowiec>
2020-06-27 16:16:25 +0200

    Repaired

--- assets/bunny/android/app/src/main/kotlin/com/example/bunny/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.bunny
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- assets/planet/README.md
+++
@@ -1,7 +0,0 @@
-# Planet
-
-Create a 3D model of the planet.
-
-## Screenshot
-
-![screenshot](../../resource/planet.gif)  
